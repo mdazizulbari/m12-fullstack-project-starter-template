27570976925521e7a60516b6d411bf284049c933
dmabari <mdazizulbarishafin@gmail.com>
2025-07-08 14:29:03 +0600

    feat: implement initial form handling for adding a new plant

--- client/src/pages/Dashboard/Seller/AddPlant.jsx
+++ client/src/pages/Dashboard/Seller/AddPlant.jsx
@@ -2,8 +2,16 @@ import AddPlantForm from "../../../components/Form/AddPlantForm";
 
 const AddPlant = () => {
   const handleFormSubmit = (e) => {
-    e.preventDefault()
-    console.log(e.target);
+    e.preventDefault();
+    const form = e.target;
+    const name = form?.name?.value;
+    const category = form?.category?.value;
+    const description = form?.description?.value;
+    const price = form?.price?.value;
+    const quantity = form?.quantity?.value;
+    const image = form?.image?.files;
+    const plantData = { name, category, description, price, quantity, image };
+    console.log(plantData);
   };
 
   return (
