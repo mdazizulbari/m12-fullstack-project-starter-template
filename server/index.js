fb6f702143672a25af38747627810c4990f5f3ce
smabari <mdazizulbarishafin@gmail.com>
2025-07-17 02:41:48 +0600

    d4 pt7

--- server/index.js
+++ server/index.js
@@ -49,114 +49,24 @@ async function run() {
   const ordersCollection = db.collection("orders");
   const usersCollection = db.collection("users");
 
-  // add a plant in db
-  app.post("/add-plant", async (req, res) => {
-    const plant = req.body;
-    const result = await plantsCollection.insertOne(plant);
-    // console.log(plant);
-    res.send(result);
-  });
-
-  // get all plants data from db
-  app.get("/plants", async (req, res) => {
-    const result = await plantsCollection.find().toArray();
-    res.send(result);
-  });
-
-  // get a single plant data from db
-  app.get("/plant/:id", async (req, res) => {
-    const id = req.params.id;
-    const result = await plantsCollection.findOne({ _id: new ObjectId(id) });
-    res.send(result);
-  });
-
-  // create payment intent for orderData
-  app.post("/create-payment-intent", async (req, res) => {
-    const { plantId, quantity } = req.body;
-    const plant = await plantsCollection.findOne({
-      _id: new ObjectId(plantId),
-    });
-    if (!plant) return res.status(404).send({ message: "Plant Not Found" });
-    const totalPrice = quantity * plant?.price * 100;
-
-    // stripe ...
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: totalPrice,
-      currency: "usd",
-      automatic_payment_methods: {
-        enabled: true,
-      },
-    });
-
-    // console.log(plantId, quantity);
-    res.send({ clientSecret: paymentIntent.client_secret });
-  });
-
-  // save or update a users info in db
-  app.post("/user", async (req, res) => {
-    const userData = req.body;
-    userData.role = "customer";
-    userData.created_at = new Date().toISOString();
-    userData.last_loggedIn = new Date().toISOString();
-
-    const query = {
-      email: userData?.email,
-    };
-    const alreadyExists = await usersCollection.findOne(query);
-
-    console.log("User already exists: ", !!alreadyExists);
-    if (!!alreadyExists) {
-      console.log("Updating user data");
-      const result = await usersCollection.updateOne(query, {
-        $set: { last_loggedIn: new Date().toISOString() },
-      });
-      return res.send(result);
-    }
-
-    // return console.log(userData);
-    console.log("Creating user data");
-    const result = await usersCollection.insertOne(userData);
-    res.send(result);
-  });
-
-  // get a user's role
-  app.get("/user/role/:email", async (req, res) => {
-    const email = req.params.email;
-    const result = await usersCollection.findOne({ email });
-    if (!result) return res.status(404).send({ message: "User not found" });
-    res.send({ role: result?.role });
-  });
-
-  // save order data in orders collection in db
-  app.post("/order", async (req, res) => {
-    const orderData = req.body;
-    const result = await ordersCollection.insertOne(orderData);
-    res.send(result);
-  });
-
-  // update plant quantity(increase/decrease)
-  app.patch("/quantity-update/:id", async (req, res) => {
-    const id = req.params.id;
-    const { quantityToUpdate, status } = req.body;
-    const filter = { _id: new ObjectId(id) };
-    let updateDoc = {
-      $inc: {
-        quantity: status === "increase" ? quantityToUpdate : -quantityToUpdate,
-      },
-    };
-    const result = await plantsCollection.updateOne(filter, updateDoc);
-    console.log(result);
-    console.log(quantityToUpdate, status);
-    res.send(result);
-  });
-
   try {
     const verifyAdmin = async (req, res, next) => {
       const email = req?.user?.email;
-      const user = await usersCollection.findOne({email});
-      if(!user||user?.role !=="admin"){
-        
-      }
+      const user = await usersCollection.findOne({ email });
+      console.log(user?.role);
+
+      if (!user || user?.role !== "admin")
+        return res.status(403).send({ message: "Admin only access!" });
+      next();
+    };
+
+    const verifySeller = async (req, res, next) => {
+      const email = req?.user?.email;
+      const user = await usersCollection.findOne({ email });
+      console.log(user?.role);
+
+      if (!user || user?.role !== "seller")
+        return res.status(403).send({ message: "Seller only access!" });
       next();
     };
 
@@ -191,8 +101,131 @@ async function run() {
       }
     });
 
+    // add a plant in db
+    app.post("/add-plant", verifyToken, verifySeller, async (req, res) => {
+      const plant = req.body;
+      const result = await plantsCollection.insertOne(plant);
+      // console.log(plant);
+      res.send(result);
+    });
+
+    // get all plants data from db
+    app.get("/plants", async (req, res) => {
+      const result = await plantsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get a single plant data from db
+    app.get("/plant/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await plantsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // create payment intent for orderData
+    app.post("/create-payment-intent", async (req, res) => {
+      const { plantId, quantity } = req.body;
+      const plant = await plantsCollection.findOne({
+        _id: new ObjectId(plantId),
+      });
+      if (!plant) return res.status(404).send({ message: "Plant Not Found" });
+      const totalPrice = quantity * plant?.price * 100;
+
+      // stripe ...
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: totalPrice,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+
+      // console.log(plantId, quantity);
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+
+    // save or update a users info in db
+    app.post("/user", async (req, res) => {
+      const userData = req.body;
+      userData.role = "customer";
+      userData.created_at = new Date().toISOString();
+      userData.last_loggedIn = new Date().toISOString();
+
+      const query = {
+        email: userData?.email,
+      };
+      const alreadyExists = await usersCollection.findOne(query);
+
+      console.log("User already exists: ", !!alreadyExists);
+      if (!!alreadyExists) {
+        console.log("Updating user data");
+        const result = await usersCollection.updateOne(query, {
+          $set: { last_loggedIn: new Date().toISOString() },
+        });
+        return res.send(result);
+      }
+
+      // return console.log(userData);
+      console.log("Creating user data");
+      const result = await usersCollection.insertOne(userData);
+      res.send(result);
+    });
+
+    // get a user's role
+    app.get("/user/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      if (!result) return res.status(404).send({ message: "User not found" });
+      res.send({ role: result?.role });
+    });
+
+    // save order data in orders collection in db
+    app.post("/order", async (req, res) => {
+      const orderData = req.body;
+      const result = await ordersCollection.insertOne(orderData);
+      res.send(result);
+    });
+
+    // get all order info for customer
+    app.get("/orders/customer/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const filter = { "customer.email": email };
+      const result = await ordersCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    // get all order info for seller
+    app.get(
+      "/orders/seller/:email",
+      verifySeller,
+      verifyToken,
+      async (req, res) => {
+        const email = req.params.email;
+        const filter = { "seller.email": email };
+        const result = await ordersCollection.find(filter).toArray();
+        res.send(result);
+      }
+    );
+
+    // update plant quantity(increase/decrease)
+    app.patch("/quantity-update/:id", async (req, res) => {
+      const id = req.params.id;
+      const { quantityToUpdate, status } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      let updateDoc = {
+        $inc: {
+          quantity:
+            status === "increase" ? quantityToUpdate : -quantityToUpdate,
+        },
+      };
+      const result = await plantsCollection.updateOne(filter, updateDoc);
+      console.log(result);
+      console.log(quantityToUpdate, status);
+      res.send(result);
+    });
+
     // get all users for admin
-    app.get("/all-users", verifyToken, async (req, res) => {
+    app.get("/all-users", verifyToken, verifyAdmin, async (req, res) => {
       console.log(req.user);
       const filter = {
         email: {
@@ -204,20 +237,25 @@ async function run() {
     });
 
     // update a user's role
-    app.patch("/user/role/update/:email", verifyToken, async (req, res) => {
-      const email = req.params.email;
-      const { role } = req.body;
-      const filter = { email: email };
-      updateDoc = {
-        $set: {
-          role,
-          status: "verified",
-        },
-      };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      console.log(role, result);
-      res.send(result);
-    });
+    app.patch(
+      "/user/role/update/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const { role } = req.body;
+        const filter = { email: email };
+        updateDoc = {
+          $set: {
+            role,
+            status: "verified",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        console.log(role, result);
+        res.send(result);
+      }
+    );
 
     // become seller request
     app.patch(
